b82bdff127971c9b7ed2ea4c14319695fbca5bc9
SushiBotz <sushil.kishore8@gmail.com>
2025-10-03 23:57:44 +0000

    Turtle Stuff

--- lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
@@ -9,7 +9,23 @@ Copy lines of code from those programs to this one to draw a triangle.
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup(600,600,0,0)               # Set the size of the window
 
-tina = turtle.Turtle()                  # Create a turtle named tina
+tina = turtle.Turtle()   
+
+tina.pencolor('blue')                   # Set the pen color to blue
+tina.forward(100)                       # Move tina forward by the forward distance
+tina.right(60)     
+
+tina.pencolor('blue')                   # Set the pen color to blue
+tina.right(100)                       # Move tina forward by the forward distance
+tina.forward(60)   
+
+tina.pencolor('blue')                   # Set the pen color to blue
+tina.right(100)                       # Move tina forward by the forward distance
+tina.right(60)       
+
+tina.pencolor('blue')                   # Set the pen color to blue
+tina.down(100)                       # Move tina forward by the forward distance
+tina.forward(60)             # Create a turtle named tina
 
 # Use tina.forward() and tina.left() to draw a triangle
 # Make each side of the triangle a different color with 
