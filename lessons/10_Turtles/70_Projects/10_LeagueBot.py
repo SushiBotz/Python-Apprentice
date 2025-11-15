b270d7124f7cc0725a8feef851be0e5b014080dc
SushiBotz <sushil.kishore8@gmail.com>
2025-11-15 00:56:09 +0000

    More and More Stuff

--- lessons/10_Turtles/70_Projects/10_LeagueBot.py
+++ lessons/10_Turtles/70_Projects/10_LeagueBot.py
@@ -10,8 +10,39 @@ Write your own turtle program! Here is what your program should do
 """
 
 import turtle as turtle
+tina = turtle.Turtle() 
+tina.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
 
+    from pathlib import Path
+    image_dir = Path(__file__).parent.parent.parent/ "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+# Set up the screen
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+
+# Create a turtle and set its shape to the custom GIF
+t = turtle.Turtle()
+
+set_turtle_image(turtle, "league_bot.gif" )
+
+tina.pendown()
+tina.pencolor('blue')  
+for i in range(6):   
+    print('Loop iteration', i)   
+    tina.forward(150)                       # Move tina forward by the forward distance
+    tina.left(60)  
+
+tina = turtle.Turtle()      
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('white')
 #(screen, "leaguebot.fig")
+
+
